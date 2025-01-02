b8da8b209f758ad30354bd2c2b5371d96c743a9e
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-01-02 00:13:47 -0500

    feat: add signin functionalioti in generate signed user operation

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -3,19 +3,34 @@ pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 
 contract SendPackedUserOp is Script {
+    using MessageHashUtils for bytes32;
+
     function run() public {}
 
-    function generatedSignedUserOperation(bytes memory callData, address sender)
+    function generatedSignedUserOperation(bytes memory callData, HelperConfig.NetworkConfig memory config)
         public
         view
         returns (PackedUserOperation memory)
     {
         // 1. Generate the unsigned data
-        uint256 nonce = vm.getNonce(sender);
-        PackedUserOperation memory unsignedUserOperation = _generateUnsignedUserOperation(callData, sender, nonce);
-        // 2. Sign the data and return it
+        uint256 nonce = vm.getNonce(config.account);
+        PackedUserOperation memory userOp = _generateUnsignedUserOperation(callData, config.account, nonce);
+
+        // 2. Get the user Op hash
+        bytes32 userOpHash = IEntryPoint(config.entryPoint).getUserOpHash(userOp);
+        bytes32 digest = userOpHash.toEthSignedMessageHash();
+
+        // 3. Sign the user Op hash
+        //Note the order is v,r,s to when sign
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(config.account, digest);
+
+        userOp.signature = abi.encodePacked(r, s, v); // Note the order is r, s, v to encode
+        return userOp;
     }
 
     function _generateUnsignedUserOperation(bytes memory callData, address sender, uint256 nonce)
